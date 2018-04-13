8216a336777b94e22cf4634c5645ee2b5f156d57
myGitHub4publicprojects <jszczep@gmail.com>
2018-04-13 17:16:52 +0200

    static files dir updated

--- ImgRate/settings.py
+++ ImgRate/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'rate/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
